92bac758bf795609d3f59e8a6582598f5f7bcfcc
Aksel Akemi Herrera Gonzalez <106628476+AkselBy18@users.noreply.github.com>
2025-10-24 09:45:11 -0700

    Documentación de dtos

--- src/modules/v1/auth/dto/login.dto.ts
+++ src/modules/v1/auth/dto/login.dto.ts
@@ -1,4 +1,16 @@
-export class LoginDto{
+import { ApiProperty } from "@nestjs/swagger";
+
+export class LoginDto {
+
+    @ApiProperty({
+        example: 'aksel@gmail.com',
+        description: 'User email'
+    })
     email: string;
+
+    @ApiProperty({
+        example: '12345678',
+        description: 'Password access for user'
+    })
     password: string;
 }
--- src/modules/v1/tasks/dto/create-task.dto.ts
+++ src/modules/v1/tasks/dto/create-task.dto.ts
@@ -1,5 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateTaskDto {
+    @ApiProperty({
+        example: 'Default Task',
+        description: 'Name for new task'
+    })
     name: string;
+
+    @ApiProperty({
+        example: 'low',
+        description: 'Type priority'
+    })
     priority: 'low' | 'medium' | 'hight';
+
+    @ApiProperty({
+        example: 'false',
+        description: 'Status for task'
+    })
     done: boolean = false
 }
