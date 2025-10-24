fb9339843be6cda2687fe5df6105b9520563d1b0
Aksel Akemi Herrera Gonzalez <106628476+AkselBy18@users.noreply.github.com>
2025-10-24 09:48:44 -0700

    Update update-task.dto.ts

--- src/modules/v1/tasks/dto/update-task.dto.ts
+++ src/modules/v1/tasks/dto/update-task.dto.ts
@@ -1,8 +1,24 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTaskDto } from './create-task.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+    @ApiProperty({
+        example: 'Default Task',
+        description: 'Name for new task'
+    })
     name?: string;
+
+    @ApiProperty({
+        example: 'low',
+        description: 'Type priority'
+    })
     priority?: 'low' | 'medium' | 'hight' | undefined;
+
+
+    @ApiProperty({
+        example: 'false',
+        description: 'Status for task'
+    })
     done?: boolean | undefined;
 }
